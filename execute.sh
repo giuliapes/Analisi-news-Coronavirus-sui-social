ee8a4303a243ca1c94ad5c9f8262821dd2226fce
giuliapes <g.pes1@studenti.unipi.it>
2020-11-24 17:49:57 +0100

    update

--- execute.sh
+++ execute.sh
@@ -7,7 +7,7 @@ has3='-h3 #tamponi'
 has4='-h4 #Pandemia'
 has5='-h5 #Coronavirus'
 UI='-UI Agenzia_Ansa'
-cd /home/giulia/Scrivania/Reazioni-news-Coronavirus-sui-social/tweetpy
+cd /home/giulia/Scrivania/Reazioni-news-Coronavirus-sui-social
 
 /usr/bin/python3 tweetpyFinal.py $has1 $has2 $has3 $has4 $has5 $UI
 
